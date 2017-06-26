9dac72d963595b41f2f8b814c506f46df80a3497
Jordan Hudgens <jordan@devcamp.com>
2017-06-26 16:56:00 +0000

    Implemented Image defaults for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -14,7 +14,7 @@ class PortfoliosController < ApplicationController
   def create
       @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
   
-      respond_to do |format|
+          respond_to do |format|
         if @portfolio_item.save
           format.html { redirect_to @portfolios_path, notice: 'Your portfolio item is now live.' }
         else
