67635e7d502c6ded63b19d627773a966d8b6fbac
Jordan Hudgens <jordan@devcamp.com>
2017-06-29 15:08:21 +0000

    Test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include DeviseWhitelist
   include SetSource
+
+  before_filter :set_title
+  
+  def set_title
+    @page_title = "Devcamp Portfolio | My Portfolio Website"
+  end 
 end
