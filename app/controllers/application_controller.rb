11df83615085ed03f4268f1ab0c2cf75354ebe2d
Jordan Hudgens <jordan@devcamp.com>
2017-07-12 00:14:52 +0000

    Integrated new gem into rails app for copyright

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,12 @@ class ApplicationController < ActionController::Base
   include DeviseWhitelist
   include SetSource
   include CurrentUserConcern
+  include DefaultPageContent
+
+  before_action :set_copyright
+
+  def set_copyright
+    @copyright = SchrieverViewTool::Renderer.copyright 'Sariah Schriever', 'All rights reserved'
+  end
 end
+
